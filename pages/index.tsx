ac067d1f4395550690944328c4e7794008dd7797
ChoiYongWon <yongwon0824@naver.com>
2022-03-01 16:22:19 +0900

    :wrench:Add Gtag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,29 @@
-import Layout from "../components/layout"
-import MapContainer from "../containers/map/MapContainer"
-import Title from "../components/title_home"
+import Layout from "../components/layout";
+import MapContainer from "../containers/map/MapContainer";
+import Title from "../components/title_home";
+import Script from "next/script";
 
-export default function Home({children}: any){
-    return (
-        <>
-        <Layout>
-            <Title/>
-            <MapContainer></MapContainer>
-        </Layout>
-        </>
-    )
+export default function Home({ children }: any) {
+	return (
+		<>
+			<Layout>
+				<Title />
+				<MapContainer></MapContainer>
+				{/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+				<Script
+					src="https://www.googletagmanager.com/gtag/js?id=G-1H07HTR0RK"
+					strategy="afterInteractive"
+				/>
+				<Script id="google-analytics" strategy="afterInteractive">
+					{`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){window.dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-1H07HTR0RK');
+                `}
+				</Script>
+			</Layout>
+		</>
+	);
 }
