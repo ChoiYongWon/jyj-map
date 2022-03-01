e8a4f0bceba47e6e309dc704b7d96564f0a8d5ff
ChoiYongWon <yongwon0824@naver.com>
2022-03-01 16:38:46 +0900

    :memo:Add description for seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,24 @@
-import type { AppProps } from "next/app"
+import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../styles/global-style";
-import darkTheme from "../styles/dark-theme"
+import darkTheme from "../styles/dark-theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <> 
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <title>정용진 부회장의 맛집 리스트</title>
-            </Head>
-            <GlobalStyle />
-            <ThemeProvider theme={darkTheme}>
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </>
-    )
-  }
+	return (
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta
+					name="description"
+					content="신세계 정용진 부회장의 전국 맛집 목록"
+				/>
+				<title>정용진 부회장의 맛집 리스트</title>
+			</Head>
+			<GlobalStyle />
+			<ThemeProvider theme={darkTheme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	);
+}
