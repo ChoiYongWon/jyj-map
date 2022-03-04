438ea5dbdc68d452c24e90da784e057c22ff5762
ChoiYongWon <yongwon0824@naver.com>
2022-03-04 14:22:29 +0900

    :sparkles:구글 seo 최적화

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,36 @@ import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../styles/global-style";
 import darkTheme from "../styles/dark-theme";
+import { DefaultSeo } from "next-seo"
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<meta
-					name="description"
-					content="신세계 정용진 부회장의 전국 맛집 목록"
-				/>
-				<title>정용진 부회장의 맛집 리스트</title>
+				<title>정용진 부회장의 전국맛집지도</title>
 			</Head>
 			<GlobalStyle />
 			<ThemeProvider theme={darkTheme}>
+				<DefaultSeo
+					title={"정용진 부회장의 전국맛집지도"}
+					description={"신세계 정용진 부회장이 선정한 전국 맛집을 알아보세요!"}
+					
+					openGraph={{
+						type: 'website',
+						locale: 'ko_KR',
+						url: 'https://jyj-map.vercel.app',
+						site_name: '정용진 전국맛집지도',
+						title: '정용진 부회장의 전국맛집지도',
+						description: '신세계 정용진 부회장이 선정한 전국 맛집을 알아보세요!',
+						images: [{
+							url: "https://raw.githubusercontent.com/ChoiYongWon/jyj-map/main/public/kakao.jpg",
+							width: 800,
+							height: 600,
+							alt: "대표 이미지"
+						}]
+					}}
+				/>
 				<Component {...pageProps} />
 			</ThemeProvider>
 		</>
